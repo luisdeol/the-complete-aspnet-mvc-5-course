867eeba8879571397a66b17f678785264656fc7f
luisdeol <luisfelipekai@gmail.com>
2017-10-08 11:04:30 -0300

    Section 5: Exercise

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -50,6 +50,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
             if (movie.Id == 0)
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,12 +9,20 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public Genre Genre { get; set; }
+
         public byte GenreId { get; set; }
         [Display(Name="Number in Stock")]
+        [Range(1,20)]
+        [Required]
         public short NumberInStock { get; set; }
         [Display(Name="Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Required]
         public DateTime? ReleaseDate { get; set; }
     }
 }
